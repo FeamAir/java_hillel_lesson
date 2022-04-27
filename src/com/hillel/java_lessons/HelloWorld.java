6c9ca6d7114187c1a4bffba07179c978a0fd6bdb
Feam <maryking.textile@gmail.com>
2022-04-27 19:19:44 +0300

    ready

---
+++ src/com/hillel/java_lessons/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hillel.java_lessons;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(" ***    *   *       *   *");
+        System.out.println("    *  * *   *     *   * *");
+        System.out.println("    * *   *   *   *   *   *");
+        System.out.println("*   * *****    * *    *****");
+        System.out.println(" ***  *   *     *     *   *");
+    }
+}
---
+++ src/com/hillel/java_lessons2/lesson_2.java
@@ -0,0 +1,66 @@
+package com.hillel.java_lessons2;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class lesson_2 {
+    public static void main(String[] args) {
+//        int i;
+//        byte b = 127;
+//        short s = 32565;
+//        char c = 65;
+//        i = 2_000_000_000;
+//        long l = 6_000_000_000L;
+//
+//        float f = 455.2456f;
+//        double e = 43569.54564;
+//
+//        boolean d = true;
+//
+//        String str="World";
+//
+//        System.out.println("c= " + c);
+//        System.out.printf("f = %.2f \n", f);
+//        System.out.printf("e = %.2f \n", e);
+//        System.out.println("c= " + d);
+//        System.out.println("c= " + str);
+//        String name = "Toyota";
+//        char category = 66;
+//            Byte wheels = 4;
+//        short passengers = 5;
+//        int number = 2;
+//        long phone = 637866576L;
+//        float price = 50.55f;
+//        double radio = 108.0;
+//        System.out.println(name);
+//        System.out.println(wheels);
+//        System.out.println(category);
+//        System.out.println(passengers);
+//        System.out.println(number);
+//        System.out.println(phone);
+//        System.out.println(price);
+//        System.out.println(radio);
+//        int a= 456;
+//        Integer i =45;
+//        Integer b =new Integer(45);
+//
+//        int g = i;
+//
+//        byte k = (byte)a;
+//        System.out.println(a);
+//        System.out.println(Integer.toBinaryString(a));
+//
+        int x = 45;
+        int y = 5;
+        int sum = x + y;
+        int sum1 = x - y;
+        int sum2 = x * y;
+        int sum4 = x / y;
+
+
+        System.out.println("x+y= " + sum);
+        System.out.println("x-y= " + sum1);
+
+
+    }
+}
+
