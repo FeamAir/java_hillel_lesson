ec538b0edb017ca21131368db2277902fe435efe
Feam <maryking.textile@gmail.com>
2022-04-27 21:59:30 +0300

    ready

---
+++ src/com/hillel/home_work2/ExeMath.java
@@ -0,0 +1,22 @@
+package com.hillel.home_work2;
+
+public class ExeMath {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 15;
+
+        int volume = x * y * z;
+        int length = 4 * (x + y + z);
+
+        float nord = 48.8582573f;
+        float east = 2.2945111f;
+
+        System.out.println("Об’єм  паралелепіпеда = " + volume);
+        System.out.println("Cумарна довжина всіх сторін = " + length);
+        System.out.printf("Ейфелева вежа — координати: " + nord + " Північної широти та " + east + " Cхідної довготи");
+
+
+    }
+
+}
