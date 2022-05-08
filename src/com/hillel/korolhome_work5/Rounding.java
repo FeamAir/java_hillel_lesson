03fa643e34b14ee0793aa08fd9c72ebe9784838e
Feam <maryking.textile@gmail.com>
2022-05-08 15:40:25 +0300

    ready

---
+++ src/com/hillel/korolhome_work5/Rounding.java
@@ -0,0 +1,28 @@
+package com.hillel.korolhome_work5;
+
+import java.util.Scanner;
+
+public class Rounding {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value ");
+        double a = scanner.nextDouble();
+        System.out.println("How many values we leave after the decimal point ??");
+        int rounding = scanner.nextInt();
+
+        double totalValue = doubleToDecimalPlaces(a,rounding);
+        System.out.println("Your value = " + totalValue);
+
+    }
+
+    private static double doubleToDecimalPlaces(double number, int countAfterPoint) {
+        String toString = Integer.toString(countAfterPoint);
+        String string = "%.";
+        String sumString = string.concat(toString).concat("f");
+        String newString = String.format(sumString, number).replace(",",".");
+        return Double.parseDouble(newString);
+
+
+    }
+}
---
+++ src/com/hillel/korolhome_work5/SerchType.java
@@ -0,0 +1,30 @@
+package com.hillel.korolhome_work5;
+
+
+import java.util.Scanner;
+
+public class SerchType {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter value ");
+
+        if (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            if (a > 0) {
+                System.out.println("Ви ввели ціле позитивне число = " + a);
+            } else {
+                System.out.println("Ви ввели ціле негативне число = " + a);
+            }
+        } else if (scanner.hasNextDouble()) {
+            double a = scanner.nextDouble();
+            System.out.printf("Ви ввели дробне число = %.2f", a);
+        } else {
+            String a = scanner.nextLine();
+            String upperCase = a.toUpperCase();
+            String replace = upperCase.replace(" ", "");
+            System.out.println("Ви ввели  строку = " + replace);
+        }
+    }
+}
